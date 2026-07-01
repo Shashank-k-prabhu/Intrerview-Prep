9411dedfa6c315f2be5126697933ff45cf19a599
Shashank K Prabhu <83872336+Shashank-k-prabhu@users.noreply.github.com>
2026-07-01 17:47:38 +0530

    Create script to merge duplicate transaction IDs
    Add a script to merge duplicate transaction IDs.

---
+++ JS-Transformations/Q9-Merge Duplicate IDs.js
@@ -0,0 +1,15 @@
+const transactions = [
+  { id: 1, amount: 100 },
+  { id: 2, amount: 200 },
+  { id: 1, amount: 300 },
+  { id: 3, amount: 150 },
+  { id: 2, amount: 100 }
+];
+
+// [
+//   { id: 1, amount: 400 },
+//   { id: 2, amount: 300 },
+//   { id: 3, amount: 150 }
+// ]
+
+
